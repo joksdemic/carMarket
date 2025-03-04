6921bcc86815d7ebceb21a4a8197073f3a095211
joksdemic <jovanademic2@gmail.com>
2025-03-04 22:00:29 +0100

    Add category list

--- src/components/Shared/Data.jsx
+++ src/components/Shared/Data.jsx
@@ -10,4 +10,13 @@ const Pricing = [
     { id: 3, amount: '30000e' }
 ];
 
-export default { CarBrands, Pricing };
+const Categories = [
+    { id: 1, name: 'Hatchback', icon:"https://img.icons8.com/?size=100&id=6htb0HQhhN2B&format=png&color=000000"},
+    { id: 2, name: 'Sedan', icon:"https://img.icons8.com/?size=100&id=16553&format=png&color=000000" },
+    { id: 3, name: 'SUV', icon:"https://img.icons8.com/?size=100&id=OVnxaMiGpJd9&format=png&color=000000" },
+    { id: 4, name: 'Convertible', icon:"https://img.icons8.com/?size=100&id=16554&format=png&color=000000" },
+    { id: 5, name: 'Van', icon:"https://img.icons8.com/?size=100&id=30485&format=png&color=000000" },
+    { id: 6, name: 'Truck', icon:"https://img.icons8.com/?size=100&id=3562&format=png&color=000000"}
+];
+
+export default { CarBrands, Pricing, Categories };
--- src/home.jsx
+++ src/home.jsx
@@ -3,12 +3,14 @@ import { SignInButton } from '@clerk/clerk-react'
 import React from 'react'
 import Header from './components/Header'
 import Hero from './components/Hero'
+import Categories from './components/Categories'
 
 function Home() {
   return (
     <div>
       <Header/>
       <Hero/>
+      <Categories/>
     </div>
   )
 }

d1b022e5acb8bc95ab304e4e2c18a124f9993ae3
joksdemic <jovanademic2@gmail.com>
2025-03-04 02:29:57 +0100

    Create header

--- src/home.jsx
+++ src/home.jsx
@@ -1,13 +1,12 @@
 import { Button } from './components/ui/button'
 import { SignInButton } from '@clerk/clerk-react'
 import React from 'react'
+import Header from './components/Header'
 
 function Home() {
   return (
     <div>
-      <SignInButton mode='modal' forceRedirectUrl='/'>
-        <Button>Sign In</Button>
-      </SignInButton>
+      <Header></Header>
     </div>
   )
 }

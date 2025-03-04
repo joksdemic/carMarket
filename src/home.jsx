baf6b7c0e2b04166e7b77450f61f60831abf969e
joksdemic <jovanademic2@gmail.com>
2025-03-04 01:34:14 +0100

    Authentication

--- src/home.jsx
+++ src/home.jsx
@@ -1,8 +1,14 @@
+import { Button } from './components/ui/button'
+import { SignInButton } from '@clerk/clerk-react'
 import React from 'react'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <SignInButton mode='modal' forceRedirectUrl='/'>
+        <Button>Sign In</Button>
+      </SignInButton>
+    </div>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Home from './home'
 import Contact from './contact'
+import { ClerkProvider } from '@clerk/clerk-react'
 
 const router = createBrowserRouter([
   {
@@ -16,8 +17,16 @@ const router = createBrowserRouter([
   }
 ])
 
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key")
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
     <RouterProvider router={router}/>
+    </ClerkProvider>
   </StrictMode>,
 )
